432a6a92b2164c1b8d635066883f3037e87ed8a0
jaassmine <jasminesharma0298@gmail.com>
2020-01-06 12:12:05 +0530

    Loading video Source

--- video_pre.py
+++ video_pre.py
@@ -3,12 +3,17 @@ import numpy as np
 
 cap = cv2.VideoCapture(0) # '0'  if there is single camera connected
                           #otherwise index of device
-
+fourcc= cv2.VideoWriter_fourcc(*'XVID')
+out = cv2.VideoWriter('output.avi' , fourcc, 20.0,(640,480))
 while True:
     ret, frame= cap.read()
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    out.write(frame)
+    cv2.imshow('gray', gray)
     cv2.imshow('frame', frame)
 
-    if cv2.waitKey(1) & 0xFF == ord('q'):  #q for quit
+    if cv2.waitKey(1) & 0xFF == ord('q'):  #'q' for quit
         break
 cap.release()
+out.release()
 cv2.destroyAllWindows()
